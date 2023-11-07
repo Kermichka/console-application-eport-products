fe1fc01299fe497142590a28d6585430aa360eab
Kermichka <anastacia.zlatinova@gmail.com>
2023-11-07 14:44:53 +0200

    added csv export and arguments

--- shopify_export_products.py
+++ shopify_export_products.py
@@ -1,20 +1,26 @@
 import shopify
 import os
+import csv
+import sys
 
-shop_url = os.getenv('MERCHANT')
+shop_url = os.getenv('SHOP_URL')
+access_token= os.getenv('ACCESS_TOKEN')
 api_version ='2023-01'
 
-API_KEY="30b2cc7f0d06481d9c061c6d6aa6dda7"
-ACCESS_TOKEN="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"
-
-session = shopify.Session(shop_url, api_version, ACCESS_TOKEN)
+session = shopify.Session(shop_url, api_version, access_token)
 shopify.ShopifyResource.activate_session(session)
 current_shop = shopify.Shop.current()
-print(current_shop)
 
 products = shopify.Product.find()
-for product in products:
-    print(product.attributes)
-else:
-    print("No products found.")
+
+arguments = sys.argv
+output_filename = arguments[1]
+
+with open(output_filename, mode='w', newline='') as csv_file:
+    fieldnames = products[0].attributes.keys()
+    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+    writer.writeheader()
+    for product in products:
+        writer.writerow(product.attributes)
+print('Ready!')
 exit(0) 
