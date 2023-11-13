1637cb90dc82c8d1c221643e9b4253bf1bec8ba8
Kermichka <anastacia.zlatinova@gmail.com>
2023-11-13 15:24:57 +0200

    Added function main

--- shopify_export_products.py
+++ shopify_export_products.py
@@ -6,44 +6,50 @@ import logging
 import argparse
 from dotenv import load_dotenv
 
-load_dotenv()
+def main():
 
-shop_url = os.getenv('SHOP_URL')
-access_token= os.getenv('ACCESS_TOKEN')
-api_version ='2023-01'
+    load_dotenv()
 
-parser = argparse.ArgumentParser(description='Shopify Data Exporter')
-parser.add_argument('-f', '--filename', help='Output filename for CSV')
-parser.add_argument('-l', '--level', help='Level of logging [DEBUG, INFO, WARNING, ERROR, CRITICAL]')
-args = parser.parse_args()
-output_filename = args.filename
-level_logging = args.level
+    shop_url = os.getenv("SHOP_URL")
+    access_token = os.getenv("ACCESS_TOKEN")
+    api_version = "2023-01"
 
-logging.basicConfig(filename='example.log', encoding='utf-8', level=level_logging)
-stderr_handler = logging.StreamHandler(sys.stderr)
-stderr_handler.setLevel(level_logging)
-logger = logging.getLogger()
-logger.addHandler(stderr_handler)
-logger = logging.getLogger()
+    parser = argparse.ArgumentParser(description="Shopify Data Exporter")
+    parser.add_argument("-f", "--filename", help="Output filename for CSV")
+    parser.add_argument(
+        "-l", "--level", help="Level of logging [DEBUG, INFO, WARNING, ERROR, CRITICAL]"
+    )
+    args = parser.parse_args()
+    output_filename = args.filename
+    level_logging = args.level
 
-logger.info('Creating shopify session...')
-session = shopify.Session(shop_url, api_version, access_token)
-shopify.ShopifyResource.activate_session(session)
-logger.info('Created shopify session.')
+    logging.basicConfig(filename="example.log", encoding="utf-8", level=level_logging)
+    stderr_handler = logging.StreamHandler(sys.stderr)
+    stderr_handler.setLevel(level_logging)
+    logger = logging.getLogger()
+    logger.addHandler(stderr_handler)
+    logger = logging.getLogger()
 
-logger.info('Getting all product data...')
-products = shopify.Product.find()
-logger.info('%i products found.', len(products))
+    logger.info("Creating shopify session...")
+    session = shopify.Session(shop_url, api_version, access_token)
+    shopify.ShopifyResource.activate_session(session)
+    logger.info("Created shopify session.")
 
-with open(output_filename, mode='w', newline='') as csv_file:
-    fieldnames = products[0].attributes.keys()
-    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-    writer.writeheader()
-    for product in products:
-        writer.writerow(product.attributes)
-logger.info('Ready! :3')
+    logger.info("Getting all product data...")
+    products = shopify.Product.find()
+    logger.info("%i products found.", len(products))
 
-# TODO 
-# functions "__name__" = "__main__"
-# black, pre-commit, flake8, from vankata
-# mypy  
+    with open(output_filename, mode="w", newline="") as csv_file:
+        fieldnames = products[0].attributes.keys()
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        writer.writeheader()
+        for product in products:
+            writer.writerow(product.attributes)
+    logger.info("Ready! :3")
+
+if __name__ == "__main__":
+
+    main()
+
+# TODO
+# mypy
