06eb540951f53f0e2f3d4f9f3d31dfc75cbb4afc
Kermichka <anastacia.zlatinova@gmail.com>
2023-11-23 00:04:02 +0200

    Reordered the code and added more stout functionality

--- shopify_export_products.py
+++ shopify_export_products.py
@@ -6,26 +6,43 @@ import logging
 import argparse
 from dotenv import load_dotenv
 
+def configure_parser():
+    parser = argparse.ArgumentParser(description="Shopify Data Exporter")
+    parser.add_argument("-f", "--filename", help="Output filename for CSV")
+    parser.add_argument(
+        "-l", "--level",
+        choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]
+    )
+    return parser
+
+def configure_logger(level_logging, log_filename="example.log"):
+    log_format = '[%(asctime)s] - %(levelname)s:%(name)s:%(message)s '
+    logging.basicConfig(filename=log_filename, encoding="utf-8", level=level_logging, format=log_format, filemode='w')
+    stderr_handler = logging.StreamHandler(sys.stderr)
+    stderr_handler.setLevel(level_logging)
+    stderr_handler.setFormatter(logging.Formatter(log_format))
+    logger = logging.getLogger()
+    logger.addHandler(stderr_handler)
+    return logger
 
 def main():
+
+    load_dotenv()
     shop_url = os.getenv("SHOP_URL")
     access_token = os.getenv("ACCESS_TOKEN")
     api_version = "2023-01"
 
-    parser = argparse.ArgumentParser(description="Shopify Data Exporter")
-    parser.add_argument("-f", "--filename", help="Output filename for CSV")
-    parser.add_argument(
-        "-l", "--level", help="Level of logging [DEBUG, INFO, WARNING, ERROR, CRITICAL]"
-    )
+    parser = configure_parser()
+    
     args = parser.parse_args()
     output_filename = args.filename
     level_logging = args.level
+    logger = configure_logger(level_logging)
+    return shop_url, access_token, api_version, output_filename, logger
 
-    logging.basicConfig(filename="example.log", encoding="utf-8", level=level_logging)
-    stderr_handler = logging.StreamHandler(sys.stderr)
-    stderr_handler.setLevel(level_logging)
-    logger = logging.getLogger()
-    logger.addHandler(stderr_handler) 
+if __name__ == "__main__":
+
+    shop_url, access_token, api_version, output_filename, logger = main()
 
     logger.info("Creating shopify session...")
     session = shopify.Session(shop_url, api_version, access_token)
@@ -42,15 +59,8 @@ def main():
         writer.writeheader()
         for product in products:
             writer.writerow(product.attributes)
+
     logger.info("Ready! :3")
 
-
-if __name__ == "__main__":
-    load_dotenv()
-    main()
-
 # TODO
-# print INFO and timestamp (same as example.log), argparse to validate argument level
-# load_dotenv() where to put settings of logger and argparser /not in main/
-# try from other file to run main()
 # mypy
